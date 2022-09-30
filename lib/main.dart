da2464263b745d6b26d0fbc3b5135234101a6207
Martenere <78728730+Martenere@users.noreply.github.com>
2022-09-30 14:04:50 +0200

    Added homescreen and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Screens/HomeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello Worldasdfa2'),
-        ),
-      ),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => const HomeScreen(),
+      },
     );
   }
 }
