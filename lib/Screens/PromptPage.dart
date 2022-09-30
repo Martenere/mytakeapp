ad9e51838f56f1ef0961b21ff2ffb0ce56ade3f1
Martenere <78728730+Martenere@users.noreply.github.com>
2022-09-30 14:19:57 +0200

    Updated content in pages

---
+++ lib/Screens/PromptPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PromptPage extends StatelessWidget {
+  const PromptPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("MYTAKE.")),
+      body: Column(children: [const SizedBox()]),
+    );
+  }
+}
