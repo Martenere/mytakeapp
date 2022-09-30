0376a1b6b701c3377dda0cd1c441bf827d674279
Martenere <78728730+Martenere@users.noreply.github.com>
2022-09-30 15:09:24 +0200

    UPDATE BRO

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -7,7 +7,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("MYTAKE.")),
-      body: Column(children: [const SizedBox()]),
+      body: Column(children: [
+        const SizedBox(
+          height: 16,
+        ),
+        GroupPane()
+      ]),
+    );
+  }
+}
+
+class GroupPane extends StatelessWidget {
+  final BoxDecoration boxstyling = BoxDecoration(border: Border.all(width: 4));
+  final TextStyle defaultText = TextStyle(fontFamily: "PTMono");
+  GroupPane({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Center(
+                child: Text(
+              "Kba KIllarna",
+              style: defaultText,
+            ))
+          ],
+        ),
+        Row(
+          children: [
+            SizedBox(
+              width: 24,
+            ),
+            Container(
+              width: 48.0,
+              height: 48.0,
+              decoration: boxstyling,
+            )
+          ],
+        ),
+        Row(),
+      ],
     );
   }
 }
