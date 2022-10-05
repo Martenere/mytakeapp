688d8e790e0d33ed53e5bc2eafb37b012387a462
klundqvist <karl.lundqvist0@gmail.com>
2022-10-05 11:00:51 +0200

    Add button to camera page hehe

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-
-  final BoxDecoration boxstyling = BoxDecoration(border: Border.all(width: 4));
-  final BoxDecoration boxstylingThick = BoxDecoration(border: Border.all(width: 8));
-  final BoxDecoration buttonStyling = BoxDecoration(border: Border.all(width: 4), color: Colors.white, boxShadow: [BoxShadow(offset: Offset(-4,4), blurRadius: 0, color: Colors.black)]);
-  final BoxDecoration backButtonStyling = BoxDecoration(border: Border.all(width: 2), color: Colors.white, boxShadow: [BoxShadow(offset: Offset(-2,2), blurRadius: 0, color: Colors.black)]);
-  final BoxDecoration boxFullstyling = BoxDecoration(color: const Color(0xFFC36AC7E), border: Border.symmetric(horizontal: BorderSide(width:3)));
-  final Color primaryColor = Color.fromARGB(255, 54, 172, 126);
-  final TextStyle defaultText = TextStyle(fontFamily: "PTMono-reg",  fontSize: 36, color: Colors.black, letterSpacing: 8);
+final BoxDecoration boxstyling = BoxDecoration(border: Border.all(width: 4));
+final BoxDecoration boxstylingThick =
+    BoxDecoration(border: Border.all(width: 8));
+final BoxDecoration buttonStyling = BoxDecoration(
+    border: Border.all(width: 4),
+    color: Colors.white,
+    boxShadow: [
+      BoxShadow(offset: Offset(-4, 4), blurRadius: 0, color: Colors.black)
+    ]);
+final BoxDecoration backButtonStyling = BoxDecoration(
+    border: Border.all(width: 2),
+    color: Colors.white,
+    boxShadow: [
+      BoxShadow(offset: Offset(-2, 2), blurRadius: 0, color: Colors.black)
+    ]);
+final BoxDecoration boxFullstyling = BoxDecoration(
+    color: const Color(0xFFC36AC7E),
+    border: Border.symmetric(horizontal: BorderSide(width: 3)));
+final Color primaryColor = Color.fromARGB(255, 54, 172, 126);
+final TextStyle defaultText = TextStyle(
+    fontFamily: "PTMono-reg",
+    fontSize: 36,
+    color: Colors.black,
+    letterSpacing: 8);
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,13 +31,28 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("MYTAKE.", style: defaultText,),  titleTextStyle: defaultText, elevation: 0, backgroundColor: Colors.white, toolbarHeight: 110,),
+      appBar: AppBar(
+        title: Text(
+          "MYTAKE.",
+          style: defaultText,
+        ),
+        titleTextStyle: defaultText,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        toolbarHeight: 110,
+      ),
       body: Column(children: [
-        
         GroupPane(groupName: "KBA killar."),
-        ElevatedButton(onPressed: (){
-          Navigator.pushNamed(context, '/PromptPage');
-        }, child: Text('Prompt page'))
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/PromptPage');
+            },
+            child: Text('Prompt page')),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/CameraPage');
+            },
+            child: Text('Camera Page'))
       ]),
     );
   }
@@ -34,7 +65,6 @@ class GroupPane extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
       decoration: boxFullstyling,
       child: Column(
         children: [
@@ -53,12 +83,16 @@ class GroupPane extends StatelessWidget {
           const SizedBox(
             height: 12,
           ),
-          Row( mainAxisAlignment: MainAxisAlignment.center,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
-              ProfileSquarePic(size:77),
-              const SizedBox(width: 24),ProfileSquarePic(size: 200,),
-              const SizedBox(width: 24), ProfileSquarePic(size:100),
+              ProfileSquarePic(size: 77),
+              const SizedBox(width: 24),
+              ProfileSquarePic(
+                size: 200,
+              ),
+              const SizedBox(width: 24),
+              ProfileSquarePic(size: 100),
             ],
           ),
           const SizedBox(
@@ -72,18 +106,20 @@ class GroupPane extends StatelessWidget {
 }
 
 class ProfileSquarePic extends StatelessWidget {
-  const ProfileSquarePic({required this.size , super.key, });
+  const ProfileSquarePic({
+    required this.size,
+    super.key,
+  });
   final int size;
 
   @override
   Widget build(BuildContext context) {
-   
     return Container(
-                width: 48.0,
-                height: 48.0,
-                decoration: boxstyling,
-                child: Image.network('https://picsum.photos/$size'),
-              );
+      width: 48.0,
+      height: 48.0,
+      decoration: boxstyling,
+      child: Image.network('https://picsum.photos/$size'),
+    );
   }
 }
 
@@ -92,12 +128,12 @@ class HardButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return InkWell(
-        child: Container(
-                  width: 48.0,
-                  height: 48.0,
-                  decoration: buttonStyling,
-                ),
-      );
+    return InkWell(
+      child: Container(
+        width: 48.0,
+        height: 48.0,
+        decoration: buttonStyling,
+      ),
+    );
   }
 }
