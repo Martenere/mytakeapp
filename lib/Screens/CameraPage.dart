f389a5399c0345166a5bec7cf001ffd8aba6f06e
klundqvist <karl.lundqvist0@gmail.com>
2022-10-05 16:51:24 +0200

    Update CameraPage
    looks horrible for now

--- lib/Screens/CameraPage.dart
+++ lib/Screens/CameraPage.dart
@@ -4,6 +4,8 @@ import 'package:mytakeapp/main.dart';
 import 'package:carbon_icons/carbon_icons.dart';
 import 'HomeScreen.dart';
 
+// https://docs.flutter.dev/cookbook/plugins/picture-using-camera
+
 class CameraPage extends StatefulWidget {
   const CameraPage({
     super.key,
@@ -15,28 +17,30 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   late CameraController controller;
+  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
     controller = CameraController(cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    }).catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-            print('User denied camera access.');
-            break;
-          default:
-            print('Handle other errors.');
-            break;
-        }
-      }
-    });
+    _initializeControllerFuture = controller.initialize();
+    // controller.initialize().then((_) {
+    //   if (!mounted) {
+    //     return;
+    //   }
+    //   setState(() {});
+    // }).catchError((Object e) {
+    //   if (e is CameraException) {
+    //     switch (e.code) {
+    //       case 'CameraAccessDenied':
+    //         print('User denied camera access.');
+    //         break;
+    //       default:
+    //         print('Handle other errors.');
+    //         break;
+    //     }
+    //   }
+    // });
   }
 
   @override
@@ -50,7 +54,9 @@ class _CameraPageState extends State<CameraPage> {
     var size = MediaQuery.of(context).size.width;
 
     if (!controller.value.isInitialized) {
-      return Container();
+      return Container(
+        child: Text('Hello'),
+      );
     }
     return Scaffold(
       backgroundColor: Colors.white,
@@ -73,48 +79,102 @@ class _CameraPageState extends State<CameraPage> {
         toolbarHeight: 110,
         foregroundColor: Colors.black,
       ),
-      body: Column(children: [
-        SizedBox(width: size, height: 30),
-        Container(
-          child: CameraPreview(controller),
-          width: size,
-          height: size,
-          decoration: boxstylingThick,
-        ),
-        SizedBox(
-          width: size,
-          height: 70,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 80,
-              height: 80,
-              child: Icon(
-                CarbonIcons.camera,
-                size: 50,
-              ),
-              decoration: buttonStyling,
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Container(
-              width: 60,
-              height: 60,
-              child: Icon(
-                CarbonIcons.flash_off,
-                size: 30,
-              ),
-              decoration: buttonStyling,
-            ), //Control panel
-          ],
-        )
-      ]),
+      body: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the Future is complete, display the preview.
+              return Column(children: [
+                SizedBox(width: size, height: 30),
+                Container(
+                  child: CameraPreview(controller),
+                  width: size,
+                  height: size,
+                  decoration: boxstylingThick,
+                ),
+                SizedBox(
+                  width: size,
+                  height: 70,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // ---- camera button -----
+                    Container(
+                      width: 80,
+                      height: 80,
+                      child: IconButton(
+                        icon: Icon(CarbonIcons.camera),
+                        onPressed: () async {
+                          // Take the Picture in a try / catch block. If anything goes wrong,
+                          // catch the error.
+                          try {
+                            // Ensure that the camera is initialized.
+                            await _initializeControllerFuture;
+
+                            // Attempt to take a picture and get the file `image`
+                            // where it was saved.
+                            final image = await controller.takePicture();
+
+                            if (!mounted) return;
+
+                            // If the picture was taken, display it on a new screen.
+                            await Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => DisplayPictureScreen(
+                                  // Pass the automatically generated path to
+                                  // the DisplayPictureScreen widget.
+                                  imagePath: image.path,
+                                ),
+                              ),
+                            );
+                          } catch (e) {
+                            // If an error occurs, log the error to the console.
+                            print(e);
+                          }
+                        },
+                      ),
+                      decoration: buttonStyling,
+                    ),
+
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      child: Icon(
+                        CarbonIcons.flash_off,
+                        size: 30,
+                      ),
+                      decoration: buttonStyling,
+                    ), //Control panel
+                  ],
+                )
+              ]);
+            } else {
+              // Otherwise, display a loading indicator.
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
 
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Text('u took a picture'),
+    );
+  }
+}
 
 //home: CameraPreview(controller),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< Updated upstream
+import firebase_auth
 import firebase_core
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-=======
+import firebase_storage
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
->>>>>>> Stashed changes
 }
