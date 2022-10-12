f2228e249225504d7ad761d569bc548d62a22972
Martenere <78728730+Martenere@users.noreply.github.com>
2022-10-12 10:27:38 +0200

    Update group_provider.dart

--- lib/Providers/group_provider.dart
+++ lib/Providers/group_provider.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-class GroupProvider with ChangeNotifier{
-  String _groupId ='';
+import '../models/modelGroup.dart';
+
+class GroupProvider with ChangeNotifier {
+  String _groupId = '';
+  late Group _group;
+
+  Group get group => _group;
   String get groupId => _groupId;
 
-void setGroupId(String groupId){
-  _groupId = groupId;
-  notifyListeners();
-}
+  void setGroupId(String groupId) {
+    _groupId = groupId;
+    notifyListeners();
+  }
+
+  void setGroup(Group group) {
+    _group = group;
+    notifyListeners();
+  }
 }
