0e5f0c588ca0daddf498ff776b34bd0d7d1aeec7
Martenere <78728730+Martenere@users.noreply.github.com>
2022-10-12 15:14:34 +0200

    asdf

---
+++ lib/loadAllGroups.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:mytakeapp/models/modelGroup.dart';
+import 'package:mytakeapp/models/modelPerson.dart';
+
+class allGroups {
+  List<Group> groups = [];
+
+  getGroupsfromFirebase(Person me) async {
+    for (var key in me.groups) {
+      Group group = await loadGroupFromFirebase(key);
+      groups.add(group);
+      print(group.name);
+    }
+    ;
+    print(groups);
+  }
+}
